e76b9647b8db4ceb7565ee66c244d0cf5d3d0cce
FedYou <federicochurruarin0@gmail.com>
2024-11-18 03:30:36 -0300

    feat: :sparkles: Added ah YouCache functionality

---
+++ index.d.ts
@@ -0,0 +1,36 @@
+declare class YouCache {
+  /**
+   * Creates a new YouCache instance
+   * @param {string} name - Name of the cache
+   **/
+  constructor(name: string);
+  /**
+   * Adds a file to the cache
+   * @param {string} filePath - Path of the file to add
+   **/
+  add(filePath: string, content: string): Promise<void>;
+  /**
+   * Create a path for the file to be cached
+   * @param {string} filePath - Path of the file to add
+   * @returns {string} - Path of the file
+   **/
+  addForPath(filePath: string): Promise<string>;
+  /**
+   * Removes a file from the cache
+   * @param {string} filePath - Path of the file to remove
+   **/
+  remove(filePath: string): Promise<void>;
+  /**
+   * Gets a file from the cache
+   * @param {string} filePath - Path of the file to get
+   * @returns {string | null} - Path of the file or null if not found
+   **/
+  get(filePath: string): Promise<string | null>;
+  /**
+   * Clears the cache
+   **/
+  clear(): void;
+}
+
+export = YouCache;
+export as namespace YouCache;
--- index.js
+++ index.js
@@ -1,4 +1,41 @@
+const fs = require("fs");
+const path = require("path");
 const youfile = require("youfile");
+const cachePath = require("./utils/cachePath");
 module.exports = class YouCache {
-  constructor() {}
+  constructor(name) {
+    if (typeof name !== "string")
+      throw new Error("YouCache: name must be a string");
+
+    this.path = cachePath(name);
+    youfile.write.dir(this.path);
+  }
+  async add(filePath, content) {
+    const hash = await youfile.get.sha256(filePath);
+    const pathCache = path.join(this.path, hash);
+    youfile.write.file(pathCache, content);
+  }
+  async addForPath(filePath) {
+    const hash = await youfile.get.sha256(filePath);
+    const pathCache = path.join(this.path, hash);
+    return pathCache;
+  }
+  async remove(filePath) {
+    const hash = await youfile.get.sha256(filePath);
+    const pathCache = path.join(this.path, hash);
+    youfile.removeExists(pathCache);
+  }
+  async get(filePath) {
+    const hash = await youfile.get.sha256(filePath);
+    const pathCache = path.join(this.path, hash);
+    if (youfile.exists(pathCache)) {
+      return pathCache;
+    } else {
+      return null;
+    }
+  }
+  clear() {
+    youfile.removeExists(this.path);
+    youfile.write.dir(this.path);
+  }
 };
