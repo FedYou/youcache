e2cd35290725d9b38f85f28ad3cd4c88c2ead1be
FedYou <federicochurruarin0@gmail.com>
2024-12-05 19:56:45 -0300

    refactor: :zap: Updated, youfile to version 1.0.10

--- index.js
+++ index.js
@@ -1,62 +1,61 @@
-const fs = require("fs");
-const path = require("path");
-const youfile = require("youfile");
-const cachePath = require("./utils/cachePath");
+const path = require('path')
+const youfile = require('youfile')
+const cachePath = require('./utils/cachePath')
 module.exports = class YouCache {
   constructor(name) {
-    if (typeof name !== "string")
-      throw new Error("YouCache: name must be a string");
-    this.path = cachePath(`${name}.node.cache`);
-    youfile.write.dir(this.path);
+    if (typeof name !== 'string')
+      throw new Error('YouCache: name must be a string')
+    this.path = cachePath(`${name}.node.cache`)
+    youfile.write.dirSync(this.path)
   }
   async add(filePath) {
-    const hash = await youfile.get.sha256(filePath);
-    const pathCache = path.join(this.path, hash);
-    youfile.copy(filePath, pathCache);
-    return pathCache;
+    const hash = await youfile.get.sha256(filePath)
+    const pathCache = path.join(this.path, hash)
+    await youfile.copy(filePath, pathCache)
+    return pathCache
   }
   async create(filePath, content) {
-    const hash = await youfile.get.sha256(filePath);
-    const pathCache = path.join(this.path, hash);
-    youfile.write.file(pathCache, content);
-    return pathCache;
+    const hash = await youfile.get.sha256(filePath)
+    const pathCache = path.join(this.path, hash)
+    await youfile.write.file(pathCache, content)
+    return pathCache
   }
   async createPath(filePath) {
-    const hash = await youfile.get.sha256(filePath);
-    const pathCache = path.join(this.path, hash);
-    return pathCache;
+    const hash = await youfile.get.sha256(filePath)
+    const pathCache = path.join(this.path, hash)
+    return pathCache
   }
   async createHash(filePath) {
-    const hash = await youfile.get.sha256(filePath);
-    return hash;
+    const hash = await youfile.get.sha256(filePath)
+    return hash
   }
   async get(filePath) {
-    const hash = await youfile.get.sha256(filePath);
-    const pathCache = path.join(this.path, hash);
-    if (youfile.exists(pathCache)) {
-      return pathCache;
+    const hash = await youfile.get.sha256(filePath)
+    const pathCache = path.join(this.path, hash)
+    if (await youfile.exists(pathCache)) {
+      return pathCache
     } else {
-      return null;
+      return null
     }
   }
   getForHash(hash) {
-    const pathCache = path.join(this.path, hash);
-    if (youfile.exists(pathCache)) {
-      return pathCache;
+    const pathCache = path.join(this.path, hash)
+    if (youfile.existsSync(pathCache)) {
+      return pathCache
     } else {
-      return null;
+      return null
     }
   }
   async remove(filePath) {
-    const hash = await youfile.get.sha256(filePath);
-    const pathCache = path.join(this.path, hash);
-    youfile.removeExists(pathCache);
+    const hash = await youfile.get.sha256(filePath)
+    const pathCache = path.join(this.path, hash)
+    await youfile.removeExists(pathCache)
   }
   delete() {
-    youfile.removeExists(this.path);
+    youfile.removeExistsSync(this.path)
   }
   clear() {
-    youfile.removeExists(this.path);
-    youfile.write.dir(this.path);
+    youfile.removeExistsSync(this.path)
+    youfile.write.dirSync(this.path)
   }
-};
+}
