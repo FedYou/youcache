40890e82c1ef3dac77fc8f9cbbf5390d6190321a
FedYou <federicochurruarin0@gmail.com>
2024-12-05 20:40:20 -0300

    feat: :zap: Some functions were changed and eliminated

--- index.js
+++ index.js
@@ -8,54 +8,32 @@ module.exports = class YouCache {
     this.path = cachePath(`${name}.node.cache`)
     youfile.write.dirSync(this.path)
   }
-  async add(filePath) {
-    const hash = await youfile.get.sha256(filePath)
-    const pathCache = path.join(this.path, hash)
-    await youfile.copy(filePath, pathCache)
-    return pathCache
-  }
-  async create(filePath, content) {
+  async add(filePath, content) {
     const hash = await youfile.get.sha256(filePath)
     const pathCache = path.join(this.path, hash)
     await youfile.write.file(pathCache, content)
-    return pathCache
-  }
-  async createPath(filePath) {
-    const hash = await youfile.get.sha256(filePath)
-    const pathCache = path.join(this.path, hash)
-    return pathCache
-  }
-  async createHash(filePath) {
-    const hash = await youfile.get.sha256(filePath)
-    return hash
+    return { hash, path: pathCache }
   }
   async get(filePath) {
     const hash = await youfile.get.sha256(filePath)
     const pathCache = path.join(this.path, hash)
     if (await youfile.exists(pathCache)) {
-      return pathCache
+      return { hash, path: pathCache }
     } else {
       return null
     }
   }
-  getForHash(hash) {
-    const pathCache = path.join(this.path, hash)
-    if (youfile.existsSync(pathCache)) {
-      return pathCache
-    } else {
-      return null
-    }
+  async create(filePath) {
+    const hash = await youfile.get.sha256(filePath)
+    return { hash, path: path.join(this.path, hash) }
   }
   async remove(filePath) {
     const hash = await youfile.get.sha256(filePath)
     const pathCache = path.join(this.path, hash)
     await youfile.removeExists(pathCache)
   }
-  delete() {
-    youfile.removeExistsSync(this.path)
-  }
-  clear() {
-    youfile.removeExistsSync(this.path)
-    youfile.write.dirSync(this.path)
+  async clear() {
+    await youfile.removeExists(this.path)
+    await youfile.write.dir(this.path)
   }
 }
