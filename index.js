29af7c342c1e4f2ae38413ec670e36298edfe73b
FedYou <federicochurruarin0@gmail.com>
2024-12-05 21:22:30 -0300

    style: :art: Changed the code style to standard

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const path = require('path')
 const youfile = require('youfile')
 const cachePath = require('./utils/cachePath')
 module.exports = class YouCache {
-  constructor(name) {
-    if (typeof name !== 'string')
-      throw new Error('YouCache: name must be a string')
+  constructor (name) {
+    if (typeof name !== 'string') { throw new Error('YouCache: name must be a string') }
     this.path = cachePath(`${name}.node.cache`)
     youfile.write.dirSync(this.path)
   }
-  async add(filePath, content) {
+
+  async add (filePath, content) {
     const hash = await youfile.get.sha256(filePath)
     const pathCache = path.join(this.path, hash)
     await youfile.write.file(pathCache, content)
     return { hash, path: pathCache }
   }
-  async get(filePath) {
+
+  async get (filePath) {
     const hash = await youfile.get.sha256(filePath)
     const pathCache = path.join(this.path, hash)
     if (await youfile.exists(pathCache)) {
@@ -23,16 +24,19 @@ module.exports = class YouCache {
       return null
     }
   }
-  async create(filePath) {
+
+  async create (filePath) {
     const hash = await youfile.get.sha256(filePath)
     return { hash, path: path.join(this.path, hash) }
   }
-  async remove(filePath) {
+
+  async remove (filePath) {
     const hash = await youfile.get.sha256(filePath)
     const pathCache = path.join(this.path, hash)
     await youfile.removeExists(pathCache)
   }
-  async clear() {
+
+  async clear () {
     await youfile.removeExists(this.path)
     await youfile.write.dir(this.path)
   }
